1ab1820062b9483da186716f4ade0e6798ade8b8
rakibul-i~ <rakibuli81336@gmail.com>
2024-01-07 23:53:01 +0600

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <h1 className="text-3xl text-center text-blue-700 text-bold">
+        This is the Starting
+      </h1>
+    </>
+  );
+};
+
+export default App;
